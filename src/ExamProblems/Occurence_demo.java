08931a3f2ca867317cae741213a95849c82b050a
vivek <vivek@Vivek>
2023-03-28 10:42:55 +0530

    Added new file Occurence_demo.java

--- src/ExamProblems/Occurence_demo.java
+++ src/ExamProblems/Occurence_demo.java
@@ -1,4 +1,22 @@
 package ExamProblems;
+import java.util.HashMap;    
 public class Occurence_demo {
-
+public static void main(String args[]) {
+	String str="Elephant";
+	
+	HashMap<Character,Integer>map=new HashMap<>();
+	for(int i=str.length()-1;i>=0;i--) {
+		if(map.containsKey(str.charAt(i))) {
+		int count=map.get(str.charAt(i));
+		map.put(str.charAt(i),count++);
+		}
+		else {
+			map.put(str.charAt(i),1);
+		}
+		
+	}
+	System.out.println(map);
+	
+	
+}
 }
